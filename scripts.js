21dde44dd1d193132a1c729d81480534c1dc8059
sh-ct <s1.turner@outlook.com>
2023-03-05 11:00:30 +0000

    added js form vai.datin and moved styling from :userinput pseudo element as it is poorly supported in browsers currently

---
+++ scripts.js
@@ -0,0 +1,89 @@
+formElement = document.querySelector('form');
+inputElements = document.querySelectorAll('.form__input');
+
+/* Helper Functions */
+
+// Returns true if the email matches the valid regex
+const emailValid = (email) => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+};
+
+const checkInput = (element) => {
+    let valid = false;
+    const elementType = element.title;
+    const value = element.value.trim();
+
+    if (value === '') {
+        onInputInvalid(
+            element,
+            `${elementType} cannot be blank`
+        );
+    }
+    else if (elementType === 'Email' && !emailValid(value)) {
+        onInputInvalid(
+            element,
+            'Looks like this is not an email'
+        );
+    }
+    else {
+        onInputValid(element);
+        valid = true;
+    }
+    return valid;
+};
+
+// Toggle valid class to invalid on the form input group and sets the content of the error message
+const onInputInvalid = (element, message) => {
+    const formField = element.parentElement;
+    formField.classList.remove('valid');
+    formField.classList.add('invalid');
+
+    const error = formField.querySelector('.error-message')
+    error.textContent = message;
+
+};
+
+// Toggle invalid class to valid on the form input and removes error message content
+const onInputValid = (element) => {
+    const formField = element.parentElement;
+    formField.classList.remove('invalid');
+    formField.classList.add('valid');
+
+    const error = formField.querySelector('.error-message');
+    error.textContent = '';
+};
+
+const debounce = (func, delay = 500) => {
+    let timeoutID;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutID) {
+            clearTimeout(timeoutID);
+        }
+        // Set new timer
+        timeoutID = setTimeout(() => {
+            func.apply(null, args)
+        }, delay);
+    };
+};
+
+/* Event Listeners */
+formElement.addEventListener('submit', (event) => {
+    console.log('form triggered');
+    event.preventDefault();
+
+    const fieldsValid = Array.from(inputElements).map(element => checkInput(element));
+    console.log('fieldsValid', fieldsValid);
+    const formValid = fieldsValid.every(field => field === true);
+    console.log('formValid', formValid);
+
+    // Submit the form in a real project, here just log to console
+    if (formValid) {
+        console.log('form submitted', event);
+    }
+});
+
+formElement.addEventListener('input', debounce((event) => {
+    checkInput(event.target);
+}))
